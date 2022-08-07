7d92bf450e4af952504c16540e6e36f8b8961565
TheEE145 <Arhitector2009@gmail.com>
2022-08-07 14:58:33 +0300

    and liquids + 1 item

---
+++ scripts/modules/elon_planer.js
@@ -0,0 +1,16 @@
+/*
+const elon = new Planet();
+const elonGenerator = new PlanetGenerator();
+
+//Planet Generator
+
+//Planet
+elon.parent = Planets.sun;
+elon.allowWaves = true;
+elon.accessible = true;
+elon.allowLaunchLoadout = true;
+elon.allowLaunchSchematics = true;
+elon.allowSectorInvasion = true;
+elon.generator = elonGenerator;
+elon.atmosphereColor = Colors.cyan;
+*/
